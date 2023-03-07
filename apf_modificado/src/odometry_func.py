9f1998a20cb019a51b85b101e3fd998204b807b0
Kelvin Nunes <kelvinkefren@gmail.com>
2023-03-07 00:54:45 -0300

    adicionado nodes do obstaculo no package apf_modificado.
    roslaunch apf_modificado obstaculo.launch

--- apf_modificado/src/odometry_func.py
+++ apf_modificado/src/odometry_func.py
@@ -5,21 +5,21 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 
-rospy.init_node('odom_box')
+rospy.init_node('odom_pub_1')
 
-odom_pub=rospy.Publisher ('/odom_box', Odometry)
+odom_pub=rospy.Publisher ('/odom_box_1', Odometry)
 
 rospy.wait_for_service ('/gazebo/get_model_state')
 get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
 odom=Odometry()
 header = Header()
-header.frame_id='/odom_box'
+header.frame_id='/odom_box_1'
 
 model = GetModelStateRequest()
-model.model_name='custom_ground_plane_box'
+model.model_name='custom_box_1'
 
-r = rospy.Rate(2)
+r = rospy.Rate(1200)
 
 while not rospy.is_shutdown():
     result = get_model_srv(model)
